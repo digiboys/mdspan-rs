8d9ec3e2ff6c2fd4b82e9ee687470fba53ab3963
Mick van Gelderen <mickvangelderen@gmail.com>
2026-03-16 11:03:18 -0700

    Add vscode configuration

---
+++ tools/vscode/find-rustfmt-path.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+workspace_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/../.." && pwd)"
+
+exec "$workspace_dir/tools/vscode/find-bazel-binary-path.bash" \
+    @rules_rust//rust/toolchain:current_rustfmt_toolchain
