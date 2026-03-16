465d22f603591d03436291e78028364e9efe2847
Oliver Lee <oliverzlee@gmail.com>
2026-03-15 21:04:50 -0700

    wipe

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,2 @@
-pub fn greeting() -> &'static str {
-    "hello from bazel rust"
-}
-
 #[cfg(test)]
-mod tests {
-    use super::greeting;
-
-    #[test]
-    fn greeting_is_stable() {
-        assert_eq!(greeting(), "hello from bazel rust");
-    }
-}
+mod tests {}
