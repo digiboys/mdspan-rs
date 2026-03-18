f41840c7797d095dd2eb36792cfb5634c9541704
Oliver Lee <oliverzlee@gmail.com>
2026-03-17 23:12:42 -0700

    tools: use custom no-fail clippy with rust-analyzer-check (#18)
    Update `rust-analyzer-check.bash` to use a wrapped version of the
    `clippy-driver` that always succeeds. This allows the clippy aspect to
    emit build failure diagnostics, even though the underlying
    `clippy-driver` returns a failure exit code.

--- tools/rust-analyzer/rust-analyzer-check.bash
+++ tools/rust-analyzer/rust-analyzer-check.bash
@@ -10,25 +10,14 @@ if [[ "$label" = "{label}" ]]; then
     label="//..."
 fi
 
-stamp_file="$(mktemp)"
-trap 'rm -f "$stamp_file"' EXIT
-
-build_status=0
 bazelisk \
   "--output_base=${output_base}" \
   build \
+  --extra_toolchains=//toolchain:local_rust_stable_clippy_no_fail \
   --aspects=//tools/rust-analyzer:aspect.bzl%clippy_stdout_aspect \
   --@rules_rust//rust/settings:error_format=json \
   --@rules_rust//rust/settings:capture_clippy_output=true \
   --output_groups=clippy_stdout \
   --config=clippy_settings \
   --keep_going \
-  -- "$label" || build_status=$?
-
-if (( build_status != 0 )); then
-    while IFS= read -r -d '' diagnostics_file; do
-        if grep -q '^{"$message_type":"diagnostic",' "$diagnostics_file"; then
-            cat "$diagnostics_file"
-        fi
-    done < <(find "$output_base" -type f -name '*.clippy.diagnostics' -newer "$stamp_file" -print0 | sort -z)
-fi
+  -- "$label" || true
